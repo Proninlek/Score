73d8b166e9f22205bb790f84a9531812c3ac055f
DFIT Vasia <pronin_lek@mail.ru>
2023-02-27 07:33:31 +0300

    Решение задачи плюс блок-схема

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿Console.Clear();
+//Console.WriteLine("Введите 5 слов или символов через запятую: ");
+string[] array1 = {"Minsk", "Pop", "2598", "2"};
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
